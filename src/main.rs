fba3a35031d92987143609414f1c15bb9edf9f42
0xhardman <1142783160@qq.com>
2024-05-31 17:52:50 +0800

    Refactor code for improved readability and maintainability

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ mod step1;
 mod step2;
 mod step3;
 mod step4;
-mod step5;
 mod step6;
+mod step5;
 mod utils;
 
 use bip39::{Language, Mnemonic, Seed};
--- src/step4.rs
+++ src/step4.rs
@@ -11,14 +11,14 @@ fn step3_master_kay_to_private_key(
 ) -> String {
     let master_secret_key_vec = hex::decode(master_secret_key_hex).unwrap();
     let master_secret_key: &[u8] = master_secret_key_vec.as_ref();
-    let master_chain_code_vec: Vec<u8> = hex::decode(master_chain_code_hex).unwrap();
-    let master_chain_code: &[u8] = master_chain_code_vec.as_ref();
+    let master_code_vec: Vec<u8> = hex::decode(master_chain_code_hex).unwrap();
+    let master_code: &[u8] = master_code_vec.as_ref();
 
     // let master_chain_code: &[u8] = hex::decode(master_chain_code_hex).unwrap().as_ref();
 
     let private_key = derive_with_path(
         SecretKey::from_slice(master_secret_key.clone()).unwrap(),
-        master_chain_code.try_into().unwrap(),
+        master_code.try_into().unwrap(),
         &derived_path,
     );
     hex::encode(private_key.as_ref())
@@ -27,10 +27,29 @@ fn step3_master_kay_to_private_key(
 #[cfg(test)]
 mod tests {
     use super::*;
+    // # step 4 - master key to private key
+    // With master key and chain code, we can derive the private key for the first account now.
+    // Let's check the derivation path first.
+    // `m / purpose' / coin_type' / account' / change / address_index`
+    // m is the master key,
+    // The purpose is 44' for BIP44, and the coin type is 0' for Bitcoin, 60' for Ethereum.
+    // The account is the index of the account, starting from 0. You can define 0 as the main account for daily use, and 1 as the account for donate or anything else.
+    // The Change field is used to differentiate between internal and external chains.
+    // The Address Index is the index of the address in the chain. You can use it to generate multiple addresses.
+    // To get the private key, we need to derive the private key from the master key with the path of each level.
+    // the derivation function is defined as:
+    // `CKDpriv((key_parent, chain_code_parent), i) -> (child_key_i, child_code_i)`
+    // `i` is the level number.
+    // CKDpriv= child key derivation (private)
+    // Itereate the derivation function with the path, we can get the private key for the first account.
+
+    // One more thing need to be mentioned is Apostrophe in the path indicates that BIP32 hardened derivation is used.
+    // such as 44' is a hardened derivation, while 44 is not.
+    // And 44' actually means 2^31+44, which is a hardened derivation.
 
     #[test]
     fn test() {
-        let derived_path = [2147483692, 2147483708, 2147483648, 0, 0];
+        let derived_path = [(2 ^ 31) + 44, (2 ^ 31) + 60, 2147483648, 0, 0];
         assert_eq!(
             step3_master_kay_to_private_key(
                 "5e01502044f205b98ba493971561284565e41f34f03494bb521654b0c35cb3a9".to_string(),
